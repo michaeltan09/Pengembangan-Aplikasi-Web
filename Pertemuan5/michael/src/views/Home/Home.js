770a43b94b03a747b86f90fd240b08647cf28237
michaeltan09 <michaelnice8899@gmail.com>
2021-11-09 19:51:33 +0700

    tugas pertemuan5

--- Pertemuan5/michael/src/views/Home/Home.js
+++ Pertemuan5/michael/src/views/Home/Home.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
 export default class Home extends React.Component{
-
+    constructor(props){
+        super(propds);
+        this.state = {name : this.props.name}
+    }
     render(){
         return(
             <div>
                 <h2>Title Home Component</h2>
-                <p>Hello my name is {this.props.name}</p>
+                <p>Hello my name is {this.state.name}</p>
+                <button onClick={()=> this.setState({name : 'Tyrion Lannister'})}>Change my name</button>
             </div>
         )
     }
+
+
+
+    // render(){
+    //     return(
+    //         <div>
+    //             <h2>Title Home Component</h2>
+    //             <p>Hello my name is {this.props.name}</p>
+    //         </div>
+    //     )
+    // }
 
 
 
