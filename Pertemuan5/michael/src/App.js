8c45514b7427e3fbcd6d79544032ca5767165733
michaeltan09 <michaelnice8899@gmail.com>
2021-11-19 19:30:34 +0700

    Pertemuan 6 - WITHROUTER

--- Pertemuan5/michael/src/App.js
+++ Pertemuan5/michael/src/App.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 import{
   BrowserRouter as Router,
   Route,
-  Link, Switch
+  Link, Switch, 
+  withRouter,
+  Redirect
 } from "react-router-dom";
 // import './css/bootstrap.min.css';
 // import './App.css';
@@ -12,20 +14,42 @@ import{
 // import Help from './views/Help/Help';
 // import Header from './header/header';
 
-class App extends Component{
 
+//ROUTER RENDER DAN REDIRECT
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      isAuth : false
+    }
+  }
   render(){
+    const loginButton = withRouter(({history}) =>( 
+    <button onClick={()=>{
+      this.setState({isAuth :true})
+      history.push('/profile')
+    }}>Login</button>
+    ))
+
+    const LogoutButton = withRouter(({history})=>(<button onClick={()=>{
+      this.setState({isAuth : false })
+      history.push('/login')
+    }}>Logout</button>
+    ))
     return(
       <Router>
         <div>
           <ul style={{listStyle: 'none'}}>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/news'>News</Link></li>
+            <li><Link to='/profile'>Profile</Link></li>
           </ul>
 
           <Switch>
             <Route path='/' exact render={()=><div>Ini adalah halaman Home</div>} />
             <Route path='/news' exact render={()=><div>Ini adalah halaman News</div>} />
+            <Route path='/login' render={()=><div><button><LoginButton /></button></div>} />
+            <Route path='/profile' render={()=> this.state.isAuth ? <div>Ini adalah halaman Profile<br /><LogoutButton /></div> : <Redirect to='/login'/>} />
           </Switch>
         </div>
       </Router>
