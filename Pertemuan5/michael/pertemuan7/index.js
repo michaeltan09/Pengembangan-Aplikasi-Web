ce57b763707f5f912711814a63505751ce5d8068
michaeltan09 <michaelnice8899@gmail.com>
2021-11-20 20:06:15 +0700

    Pertemuan 7 - bodyparser

--- Pertemuan5/michael/pertemuan7/index.js
+++ Pertemuan5/michael/pertemuan7/index.js
@@ -1,16 +1,22 @@
 var http = require('http');
+var express = require('express');
+var app = express()
+var bodyParser = require('body-parser')
 
-http.createServer((req,res)=>{
-    if (req.url == '/home') {
-        res.write('<p>Konten Home</p>')
-    }else if (req.url == '/product') {
-        res.write('<p>Konten Product</p>')
-    }else if (req.url == '/order') {
-        res.write('<p>Konten Order</p>')
-    }else if (req.url == '/') {
-        res.write('<p>Pengembangan Aplikasi Web dengan Nodejs</p>')
-    }else
-    res.end();
-}).listen(8000,()=>{
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+var persons = [];
+
+app.post('/person',(req,rest)=>{
+    persons.push(req.body)
+    res.json(req.body)
+})
+
+app.get('/person',(req,rest)=>{
+    rest.json(persons)
+})
+
+http.createServer(app).listen(8000,()=>{
     console.log('Server is running on port 8000')
 })
